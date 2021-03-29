a37ace8af3731ab6425ce3137ba72d3c22b55a85
Andy Gardner <andy.gardner@uconn.edu>
2021-03-29 00:11:31 -0400

    1st commit to sandbox

---
+++ SANDBOX/Week-10_clean/js/main.js
@@ -0,0 +1,78 @@
+//Variables
+const api_end = 'https://github.uconn.edu/api/v3/search/users?q=Andy';
+const api_test = './json/users.json';
+var count = document.querySelector('.count');
+//var user = document.querySelector('input');
+//user.addEventListener('change', getUsers);
+var button = document.querySelector('#submit');
+button.addEventListener('click', getUsers);
+
+//Start of new block of code...
+const formData = document.querySelector('.submit');
+
+formData.addEventListener('submit', function(e){
+    e.preventDefault();
+
+    const userData = new  UserData(this);
+
+    fetch('./login.php', {
+        method: 'post',
+        body: userData
+    }).then(function (response) {
+        return response.text();
+    }).then(function (text) {
+        console.log(text);
+    }).catch(function (error) {
+        console.log(error);
+    })
+});
+//End of code...
+function getUsers() {
+    let x = document.getElementById("users").value;
+    document.getElementById("userResult").innerHTML = x;
+    }
+
+ 
+
+//async
+async function mySearch() {
+    const response = await fetch(api_end);
+    const object =  await response.json();
+    const items = await object.items;
+   /*  console.log(response);
+    console.log(object.total_count);
+    console.log(items.length);
+    console.log(object);
+    console.log(items); */
+    document.querySelector('.count').innerHTML = object.total_count + ' Search results';
+    
+ 
+    items.forEach(element => {
+        console.log(element.avatar_url);
+        
+        
+        document.getElementById('userResult').innerHTML += `
+        <ul>
+            <li>Login: ${element.login}</li>
+            <li>User page: <a href="${element.html_url}" title="${items.title}">${element.html_url}</a></li>
+            <img src="${element.avatar_url}" alt="" width="100" height="100"> 
+        </ul>
+        
+        `;
+        
+        element.login;
+        element.html_url;
+    });
+    
+    
+}
+
+mySearch();
+
+
+
+
+
+
+
+
